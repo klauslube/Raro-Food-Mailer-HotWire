fa0eeb698816185cebfbcbe54c68201032ff95ce
klauslube <klauslube93@gmail.com>
2023-05-16 12:30:28 -0300

    feat: add routes '/chefs'

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+Rails.application.routes.draw do 
+  resources :chefs, only: %i[show index] do 
+    member do
+      resource :address, only: %i[show]       # '/chefs/:chef_id/address'
+      resource :city, only: %i[show]          # '/chefs/:chef_id/city'  
+      resource :state, only: %i[show]         # '/chefs/:chef_id/state'
+      resources :telephones, only: %i[index]  # '/chefs/:chef_id/telephones'
+      resources :dishes, only: %i[index]      # '/chefs/:chef_id/dishes'
+    end
+  end
 end
