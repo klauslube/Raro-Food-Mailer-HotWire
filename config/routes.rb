d7fc7d1a1188adecb9b30b85fef836c1cb1fc21b
klauslube <klauslube93@gmail.com>
2023-05-16 14:00:19 -0300

    feat: add routes for states

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do 
-  resources :chefs, only: %i[show index] do 
+  resources :chefs do 
     member do
       resource :address, only: %i[show]       # '/chefs/:chef_id/address'
       resource :city, only: %i[show]          # '/chefs/:chef_id/city'  
       resource :state, only: %i[show]         # '/chefs/:chef_id/state'
       resources :telephones, only: %i[index]  # '/chefs/:chef_id/telephones'
       resources :dishes, only: %i[index]      # '/chefs/:chef_id/dishes'
+    end
+  end
+  
+  resources :states do
+    member do 
+      resources :city, only: %i[index] 
     end
   end
 end
