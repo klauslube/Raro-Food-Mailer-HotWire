7b7a13d43325531f72de79414a1ebe008014be7b
klauslube <klauslube93@gmail.com>
2023-05-24 16:24:46 -0300

    feat: adiciona devise ao model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  validates :name, :email, :cpf, :password, presence: true
-  validates :cpf, length: { is: 11 }
-  validates :cpf, uniqueness: { scope: :email }
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  # validates :name, :email, :cpf, :password, presence: true
+  # validates :cpf, length: { is: 11 }
+  # validates :cpf, uniqueness: { scope: :email }
   validates :password, length: { in: 6..20 }
 end
